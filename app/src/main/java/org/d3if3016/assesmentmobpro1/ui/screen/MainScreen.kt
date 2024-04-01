7ce13b00b5e358c167a0e5c14d8b4186f44736ee
Dyna Rosalina Pangaribuan <pangaribuandyna@gmail.com>
2024-04-01 14:17:58 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/MainScreen.kt
@@ -1,9 +1,12 @@
-package org.d3if3016.assesmentmobpro1
+package org.d3if3016.assesmentmobpro1.ui.screen
+
+import org.d3if3016.assesmentmobpro1.R
 
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,9 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import org.d3if3016.assesmentmobpro1.ui.screen.MainScreen
+import androidx.compose.ui.unit.dp
 import org.d3if3016.assesmentmobpro1.ui.theme.AssesmentMobpro1Theme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,43 +38,42 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun MainScreen(content: @Composable (Modifier) -> Unit) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = stringResource(id = R.string.app_name))
-                    },
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    )
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
                 )
-            }
-        ) { padding ->
-            content(Modifier.padding(padding))
-        }
-    }
-
-    @Composable
-    fun Greeting(name: String) {
-        MainScreen { modifier ->
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
             )
         }
+    ) { padding ->
+        ScreenContent(Modifier.padding(padding))
     }
+}
 
-    @Preview(showBackground = true)
-    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-    @Composable
-    fun ScreenPreview() {
-        AssesmentMobpro1Theme {
-            Greeting("Android")
-        }
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenPreview() {
+    AssesmentMobpro1Theme {
+        MainScreen()
     }
 }
