ee08fa9e6febce3a1aa47a96de4f8b34752ada5c
Dyna Rosalina Pangaribuan <pangaribuandyna@gmail.com>
2024-04-02 17:50:17 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/AboutScreen.kt
@@ -1,7 +1,12 @@
 package org.d3if3016.assesmentmobpro1.ui.screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -10,15 +15,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3016.assesmentmobpro1.R
+import org.d3if3016.assesmentmobpro1.ui.theme.AssesmentMobpro1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.kembali),
+                            tint = Color.White
+                        )
+                    }
+                },
                 title = {
                     Text(text = stringResource(id = R.string.tentang_aplikasi))
                 },
@@ -33,5 +51,14 @@ fun AboutScreen() {
             text = stringResource(id = R.string.copyright),
             modifier = Modifier.padding(padding).padding(16.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun AboutScreenPreview() {
+    AssesmentMobpro1Theme {
+        AboutScreen(rememberNavController())
     }
 }
