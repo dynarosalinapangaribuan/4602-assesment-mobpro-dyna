d33e3fe97156eda521671cb17e1782626356bf95
Dyna Rosalina Pangaribuan <pangaribuandyna@gmail.com>
2024-06-11 23:32:01 +0700

    Membuat tampilan nama tanaman

--- app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3016/assesmentmobpro1/ui/screen/MainScreen.kt
@@ -4,8 +4,10 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,13 +23,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -79,7 +85,9 @@ fun ScreenContent(modifier: Modifier) {
     val data by viewModel.data
 
     LazyVerticalGrid(
-        modifier = modifier.fillMaxSize().padding(4.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(4.dp),
         columns = GridCells.Fixed(2)
     ) {
         items(data) { ListItem(tanaman = it) }
@@ -89,7 +97,10 @@ fun ScreenContent(modifier: Modifier) {
 @Composable
 fun ListItem(tanaman: Tanaman) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier
+            .padding(4.dp)
+            .border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -98,8 +109,27 @@ fun ListItem(tanaman: Tanaman) {
                 .build(),
             contentDescription = stringResource(id = R.string.gambar, tanaman.nama),
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().padding(4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
         )
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+                .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ) {
+            Text(
+                text = tanaman.nama,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+                )
+            Text(
+                text = tanaman.namaLatin,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+                )
+        }
     }
 }
 
